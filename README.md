145bff13b97881a38239b9e9d12059fc92a72264
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-07-21 18:25:41 +0100

    to be amended

---
+++ README.md
@@ -0,0 +1,21 @@
+## aft-workshop-sample
+
+Sample repository and customization for AWS Control Tower Account Factory for Terraform (AFT).
+
+This repository is part of AFT lab. For detailed walkthrough, please check the lab in [AWS Control Tower Workshop - Account Factory for Terraform (AFT) ](https://catalog.workshops.aws/control-tower/en-US/customization/aft).
+
+## How to use 
+
+There are four branches in this repository, navigate to each branch for more content. Refer to instruction on [AWS Control Tower Workshop](https://catalog.workshops.aws/control-tower/en-US) for detailed walkthrough.
+
+## Branch: aft-cloud9 
+
+Use this example to automate Cloud9 environment setup with Terraform and bash. The Cloud9 environment will have IAM role attached with permissions to launch AFT.
+
+## Security
+
+See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = var.ct_home_region
+}
+
+resource "random_id" "id" {
+  byte_length = 8
+}
+
+resource "aws_iam_role" "cloud9-aft-role" {
+  name = "cloud9-aft-role-${random_id.id.hex}"
+  path = "/"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AdministratorAccess"]
+}
+
+resource "aws_iam_instance_profile" "cloud9-aft-profile" {
+  name = var.c9_instance_profile
+  role = aws_iam_role.cloud9-aft-role.name
+}
+
+
+resource "aws_ec2_instance_metadata_defaults" "enforce-imdsv2" {
+  http_endpoint = "enabled"
+  http_tokens = "required"
+}
+
+resource "aws_cloud9_environment_ec2" "cloud9-aft" {
+  name          = var.c9_instance_name
+  image_id      = "amazonlinux-2-x86_64"
+  instance_type = "t3.small"
+  subnet_id     = local.vpc.is_use_aft_vpc ? data.aws_subnet.aft_public_subnet_01[local.vpc.index].id : null
+}
